41576dcb57289a468b618818fcc8e8d52f94f429
xubing <447601568@qq.com>
2014-06-05 16:19:32 +0800

    Initial Commit

---
+++ TableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableView
+//
+//  Created by xubing on 14-6-5.
+//  Copyright (c) 2014年 xubing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewTests/TableViewTests.m
@@ -0,0 +1,34 @@
+//
+//  TableViewTests.m
+//  TableViewTests
+//
+//  Created by xubing on 14-6-5.
+//  Copyright (c) 2014年 xubing. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableViewTests : XCTestCase
+
+@end
+
+@implementation TableViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
